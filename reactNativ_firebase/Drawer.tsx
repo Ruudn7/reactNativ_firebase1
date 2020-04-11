95502a971979ac302d932dbf366e95f2e605c3e5
Sławomir <ruudn7@wp.pl>
2020-04-11 16:59:53 +0200

    add stack and show menu button

--- reactNativ_firebase/Drawer.tsx
+++ reactNativ_firebase/Drawer.tsx
@@ -1,5 +1,9 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import React from 'react';
+
+import { Block, Text, Button } from 'expo-ui-kit';
 
 import Contact from './screens/Contact';
 import Dashboard from './screens/Dashboard';
@@ -7,12 +11,31 @@ import Messages from './screens/Messages';
 
 const Drawer = createDrawerNavigator();
 
+const Stack = createStackNavigator();
+
+const Screens: any = ({ navigation  }) => {
+    return (
+        <Stack.Navigator screenOptions={{
+            headerTransparent: true,
+            headerTitle: '',
+            headerLeft: () => (
+                <Button primary padding marginHorizontal onPress={() => navigation.openDrawer()}>
+                    <Text white small>Menu</Text>
+                </Button>
+            )
+        }}>
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen name="Messages" component={Messages} />
+            <Stack.Screen name="Contact" component={Contact} />
+        </Stack.Navigator>
+    )
+
+}
+
 export default () => {
     return(
         <Drawer.Navigator initialRouteName="Dashboard">
-            <Drawer.Screen name="Dashboard" component={Dashboard} />
-            <Drawer.Screen name="Messages" component={Messages} />
-            <Drawer.Screen name="Contact" component={Contact} />
+            <Drawer.Screen name="Screens" component={Screens} />
         </Drawer.Navigator>
     )
 }
