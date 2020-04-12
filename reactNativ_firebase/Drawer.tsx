25ddadeff93db9c4df83f37d3c69f2f4c7b88144
Sławomir <ruudn7@wp.pl>
2020-04-12 02:30:16 +0200

    finish side menu app

--- reactNativ_firebase/Drawer.tsx
+++ reactNativ_firebase/Drawer.tsx
@@ -1,6 +1,7 @@
 import { Feather } from '@expo/vector-icons';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
+import { LinearGradient } from 'expo-linear-gradient'
 import { Block, Button, Text } from 'expo-ui-kit';
 import React from 'react';
 import { Image } from 'react-native';
@@ -8,38 +9,42 @@ import { Image } from 'react-native';
 import Contact from './screens/Contact';
 import Dashboard from './screens/Dashboard';
 import Messages from './screens/Messages';
+import Animated from 'react-native-reanimated';
 
 const Drawer = createDrawerNavigator();
 
 const Stack = createStackNavigator();
 
-const Screens: any = ({ navigation  }) => {
+const Screens: any = ({ navigation, style  }) => {
     return (
-        <Stack.Navigator screenOptions={{
-            headerTransparent: true,
-            headerTitle: '',
-            headerLeft: () => (
-                <Button
-                    padding
-                    transparent
-                    marginHorizontal
-                    onPress={() => navigation.openDrawer()}
-                >
-                    <Feather name="menu" size={18} />
-                </Button>
-            )
-        }}>
-            <Stack.Screen name="Dashboard" component={Dashboard} />
-            <Stack.Screen name="Messages" component={Messages} />
-            <Stack.Screen name="Contact" component={Contact} />
-        </Stack.Navigator>
+        <Animated.View style={[{overflow: 'hidden', flex: 1}, style]}>
+            <Stack.Navigator screenOptions={{
+                headerTransparent: true,
+                headerTitle: '',
+                headerLeft: () => (
+                    <Button
+                        padding
+                        transparent
+                        marginHorizontal
+                        onPress={() => navigation.openDrawer()}
+                    >
+                        <Feather name="menu" size={18} />
+                    </Button>
+                )
+            }}>
+                <Stack.Screen name="Dashboard" component={Dashboard} />
+                <Stack.Screen name="Messages" component={Messages} />
+                <Stack.Screen name="Contact" component={Contact} />
+            </Stack.Navigator>
+        </Animated.View>
+
     )
 
 }
 
 const DrawerContent = (props: any) => {
     return (
-        <DrawerContentScrollView {...props}>
+        <DrawerContentScrollView {...props} contentContainerStyle={{flex: 1}}>
             <Block>
                 <Block flex={0.4} margin={20} bottom>
                     <Image
@@ -49,8 +54,8 @@ const DrawerContent = (props: any) => {
                         resizeMode='center'
                         style={{ borderRadius: 30}}
                     />
-                    <Text title>MENU REACT NATIVE</Text>
-                    <Text size={9}>sidebar</Text>
+                    <Text title marginTop='2x' >MENU REACT NATIVE</Text>
+                    <Text size={9} marginTop>sidebar</Text>
                 </Block>
                 <DrawerItem
                     label="Dashboard"
@@ -71,15 +76,57 @@ const DrawerContent = (props: any) => {
                     icon={()=> <Feather name="phone" color="red" size={16} />}
                 />
             </Block>
+
+            <Block noflex>
+                <DrawerItem
+                    label="Logout"
+                    labelStyle={{marginLeft: -16}}
+                    onPress={() => alert('Are you sure?')}
+                    icon={()=> <Feather name="log-out" color="red" size={16} />}
+                />
+            </Block>
             
         </DrawerContentScrollView>
         );
 }
 
 export default () => {
+    const [progress, setProgress] = React.useState(new Animated.Value(0)) as any;
+
+    const scale = Animated.interpolate(progress, {
+        inputRange: [0,1],
+        outputRange: [1, 0.8]
+    })
+
+    const borderRadius = Animated.interpolate(progress, {
+        inputRange: [0,1],
+        outputRange: [0, 10]
+    })
+
+    const screenStyles = {borderRadius, transform: [{scale}]};
+
     return(
-        <Drawer.Navigator initialRouteName="Dashboard" drawerContent={props => <DrawerContent {...props } />}>
-            <Drawer.Screen name="Screens" component={Screens} />
-        </Drawer.Navigator>
+            <LinearGradient style={{flex: 1}} colors={['white', 'gray']}>
+                <Drawer.Navigator
+                    drawerType="slide"
+                    overlayColor="transparent"
+                    initialRouteName="Dashboard"
+                    drawerStyle={{ width: '50%', backgroundColor: 'transparent' }}
+                    drawerContentOptions={{
+                        activeBackgroundColor: "transparent",
+                        activeTintColor: 'green',
+                        inactiveTintColor: 'green',
+                    }}
+                    sceneContainerStyle={{backgroundColor: "transparent"}}
+                    drawerContent ={ props => {
+                        setProgress(props.progress);
+                        return <DrawerContent {...props }/>
+                    }}>
+                    <Drawer.Screen name="Screens">
+                        {props => <Screens {...props} style={screenStyles} />}
+                    </Drawer.Screen>
+                </Drawer.Navigator>
+            </LinearGradient>
+
     )
 }
